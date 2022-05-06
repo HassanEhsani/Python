94c18968222722e4b7e1b209e64ae4668e972e95
Hassan1Ehsani <hassan.ehsani.am@gmail.com>
2022-05-06 11:33:03 +0300

    write code page 121 Q (C)

---
+++ Python - часть 2/page_121_qC.py
@@ -0,0 +1,11 @@
+def printNum(x,n):
+    if x==0: return
+    printNum(x//16,n)
+    if x%n<10:
+        print(x%n, end="")
+    else:
+        print(mas[x%n], end="")
+mas="0123456789ABCDEF"
+x = int(input("enter the Decimal number: "))
+# n = int(input("Введте основание системы счисления (2-16):"))
+printNum(x,16)
