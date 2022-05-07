bfee353a11c8ff57a69c69ccfa70b537c5c500c8
Hassan1Ehsani <hassan.ehsani.am@gmail.com>
2022-05-07 15:10:35 +0300

    return statemenr

--- Co_worker.ITMO/add_parameter_in_function.py
+++ Co_worker.ITMO/add_parameter_in_function.py
@@ -1,8 +1,9 @@
-def greet_user(name):
-    print(f"Hi {name}!")
+def greet_user(first_name,last_name):
+    print(f"Hi {first_name} {last_name}!")
     print("Welcone aboard.")
 
 
 print("Start")
-greet_user("Hassan")
+greet_user("Hassan","Ehsani")
+# greet_user("Mary")
 print("Finish")
---
+++ Co_worker.ITMO/return_statement.py
@@ -0,0 +1,8 @@
+from unittest import result
+
+
+def square(number):
+    return number * number
+
+
+print(square(3))
