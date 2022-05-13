c8db2e1392ad6a903cc64596b22661513eefef6f
Hassan1Ehsani <hassan.ehsani.am@gmail.com>
2022-05-13 12:58:26 +0300

    Q12

---
+++ Информатика РФ2021 - 1 вариант для тестирования/page_6_Задание 12.py
@@ -0,0 +1,8 @@
+min = 555556
+q = 0
+for x in range (100,555555):
+    if(x%12==0 or x%15!=0)and (x%8==7 or (x%8==6 and (x//8)%8==6)):
+        q +=1
+        if min>x:
+            min=x
+print(q,min**2)
